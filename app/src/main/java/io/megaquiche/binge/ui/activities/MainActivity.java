c77d982f54d4d6214246ab2c8fa5af93198417ad
plsr <christianpoplawski@gmail.com>
2016-05-26 22:41:00 +0200

    Fixed package name for main activity

--- app/src/main/java/io/megaquiche/binge/ui/activities/MainActivity.java
+++ app/src/main/java/io/megaquiche/binge/ui/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package io.megaquiche.binge;
+package io.megaquiche.binge.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import io.megaquiche.binge.R;
 
 public class MainActivity extends AppCompatActivity {
 
