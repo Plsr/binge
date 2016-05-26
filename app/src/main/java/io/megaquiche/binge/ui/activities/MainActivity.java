e6b4e559f27dcfe2abf25a54886d1d05420381c1
plsr <christianpoplawski@gmail.com>
2016-05-26 22:53:02 +0200

    Initialized basic toolbar in Main Activity

--- app/src/main/java/io/megaquiche/binge/ui/activities/MainActivity.java
+++ app/src/main/java/io/megaquiche/binge/ui/activities/MainActivity.java
@@ -2,14 +2,26 @@ package io.megaquiche.binge.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import io.megaquiche.binge.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up the Toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Meine Serien");
+
+        setSupportActionBar(toolbar);
+
+
+
     }
 }
