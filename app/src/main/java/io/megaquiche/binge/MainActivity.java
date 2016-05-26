7b2eb527a5fba8fdaf08a4ffc954b6216d8ab307
plsr <christianpoplawski@gmail.com>
2016-05-26 19:09:07 +0200

    Initial Commit
    All hail MegaQuiche

---
+++ app/src/main/java/io/megaquiche/binge/MainActivity.java
@@ -0,0 +1,13 @@
+package io.megaquiche.binge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
