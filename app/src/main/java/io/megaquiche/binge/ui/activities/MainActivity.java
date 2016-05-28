6dff6b00da04c9458c1a04f085b423b3c5d0022c
plsr <christianpoplawski@gmail.com>
2016-05-28 14:18:04 +0200

    Set color of StatusBar to primeryDark

--- app/src/main/java/io/megaquiche/binge/ui/activities/MainActivity.java
+++ app/src/main/java/io/megaquiche/binge/ui/activities/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.Window;
+import android.view.WindowManager;
+import android.app.Activity;
 
 import io.megaquiche.binge.R;
 
@@ -26,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        // Set StatusBar Color
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
     }
 
     @Override
