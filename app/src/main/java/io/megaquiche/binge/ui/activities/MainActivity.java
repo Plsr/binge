bc84ef9d29fb5d0a787eed21516f4ac920fbef99
plsr <christianpoplawski@gmail.com>
2016-05-29 16:57:38 +0200

    Switched Button to ImageButton
    Also, added onClickListener in MainActivity

--- app/src/main/java/io/megaquiche/binge/ui/activities/MainActivity.java
+++ app/src/main/java/io/megaquiche/binge/ui/activities/MainActivity.java
@@ -1,19 +1,25 @@
 package io.megaquiche.binge.ui.activities;
 
+import android.provider.Settings;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import io.megaquiche.binge.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +43,15 @@ public class MainActivity extends AppCompatActivity {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimaryDark));
+
+        ImageButton mark_watched = (ImageButton) findViewById(R.id.action_mark_watched);
+        mark_watched.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // TODO: Mark latest unseen epsiode as seen and display next unseen Episode
+                Log.d(TAG, "mark_watched onClick listener called");
+            }
+        });
+
     }
 
     @Override
