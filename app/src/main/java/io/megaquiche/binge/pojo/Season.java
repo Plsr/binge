fa0a955e135b8a145cfc2376f243a10395e9608d
Timo Mämecke <maemecketimo@gmail.com>
2016-05-29 21:40:28 +0200

    Season gson pojo

---
+++ app/src/main/java/io/megaquiche/binge/pojo/Season.java
@@ -0,0 +1,46 @@
+package io.megaquiche.binge.pojo;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Timo Maemecke (@timomeh) on 29/05/16.
+ * <p/>
+ * TODO: Add a class header comment
+ */
+public class Season {
+    @SerializedName("season_number") private int mNumber;
+    @SerializedName("id") private int mId;
+    @SerializedName("episodes") private List<Episode> mEpisodeList;
+
+    public Season(int number, int id, List<Episode> episodeList) {
+        mNumber = number;
+        mId = id;
+        mEpisodeList = episodeList;
+    }
+
+    public int getNumber() {
+        return mNumber;
+    }
+
+    public void setNumber(int number) {
+        mNumber = number;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public List<Episode> getEpisodeList() {
+        return mEpisodeList;
+    }
+
+    public void setEpisodeList(List<Episode> episodeList) {
+        mEpisodeList = episodeList;
+    }
+}
