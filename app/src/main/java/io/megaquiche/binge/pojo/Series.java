c5c4f4876bee1d76f533e872333dd78b6ad9a53a
Timo Mämecke <maemecketimo@gmail.com>
2016-05-29 20:23:47 +0200

    GSON placeholder

--- app/src/main/java/io/megaquiche/binge/pojo/Series.java
+++ app/src/main/java/io/megaquiche/binge/pojo/Series.java
@@ -1,9 +1,13 @@
 package io.megaquiche.binge.pojo;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Timo Maemecke (@timomeh) on 27/05/16.
  * <p/>
  * TODO: Add a class header comment
  */
 public class Series {
+    @SerializedName("seriesName") String mName;
+    @SerializedName("overview") String mDescription;
 }
