f724dfca88c483f648241bcd8add9144083fd451
Timo Mämecke <maemecketimo@gmail.com>
2016-06-01 22:49:29 +0200

    Merged from pojo-json at 5fea601

--- app/src/main/java/io/megaquiche/binge/utils/APIRequest.java
+++ app/src/main/java/io/megaquiche/binge/utils/APIRequest.java
@@ -24,50 +24,7 @@ import java.util.List;
  * TODO: Add a class header comment
  */
 public class APIRequest {
-    private final static String TAG = APIRequest.class.getSimpleName();
 
-    // FIXME: Duplicate from JWTRequest
-    private final static String THETVDB_URL = "https://api.thetvdb.com";
-
-    private RequestQueue mQueue;
-    private Context mContext;
-
-    public APIRequest(Context context) {
-        mContext = context;
-        mQueue = Volley.newRequestQueue(context);
-    }
-
-    public interface Result {
-        void onSuccess(List<?> results);
-        void onFailure();
-    }
-
-    public void searchSeries(String query, final Result res) {
-        String qs = "";
-        try {
-            qs = "?name=" + URLEncoder.encode(query, "utf-8");
-        } catch (UnsupportedEncodingException e) {
-            Log.e(TAG, "Cannot encode this with utf-8", e);
-            e.printStackTrace();
-        }
-
-        JsonObjectRequest searchRequest = new JsonObjectRequest(Request.Method.GET,
-                THETVDB_URL + "/search/series" + qs, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        res.onSuccess(response.getJSONArray());
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        res.onFailure();
-                    }
-                });
-
-        mQueue.add(searchRequest);
-    }
 
 
 }
