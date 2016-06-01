7c3d62e20f2160061645630507b7364b9f7a6bac
Timo Mämecke <maemecketimo@gmail.com>
2016-06-01 19:36:12 +0200

    Forgot Series.ImageUrl Test

--- app/src/test/java/io/megaquiche/binge/pojo/SeriesTest.java
+++ app/src/test/java/io/megaquiche/binge/pojo/SeriesTest.java
@@ -15,13 +15,14 @@ public class SeriesTest {
     @Test
     public void parsing_withoutSeasons_isWorking() throws Exception {
         String json = "{'name': 'Series Name', 'id': 941, 'overview': 'This is a description', "+
-                "'backdrop_path': 'image.png'}";
+                "'backdrop_path': '/image.jpg'}";
         Gson gson = new Gson();
         Series series = gson.fromJson(json, Series.class);
 
         assertEquals(941, series.getId());
         assertEquals("Series Name", series.getName());
         assertEquals("This is a description", series.getDescription());
+        assertEquals(Series.TMDB_BACKDROP_PREFIX + "/image.jpg", series.getImageUrl());
         assertNull(series.getSeasons());
     }
 }
