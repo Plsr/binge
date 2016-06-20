61f80189b48a8f2036ddc7d2cdd8791fbb618702
Christian Poplawski <christianpoplawski@gmail.com>
2016-06-20 10:26:56 +0200

    Main Activity Visual (#3)
    * Refactored main activity and removed default actionbar
    
    * Fixed package name for main activity
    
    * Initialized basic toolbar in Main Activity
    
    * Refactored Toolbar to be compatible with the menu drawer later on
    
    * Added structure for drawables
    
    * Added search icon
    
    * Added XML for home menu
    
    * Toolbar appearance finished
    
    Toolbar itself is non-functional though.
    
    * Set color of StatusBar to primeryDark
    
    * Tidied up and befriended the compiler
    
    * Basic card structure
    
    * Styling for Series Cards done
    
    Like the Menu, still non-functional
    
    * Improved checkmark icon
    
    * Switched Button to ImageButton
    
    Also, added onClickListener in MainActivity
    
    * Removed unnecessary imports
    
    * Finished temporary Adapter for RecyclerView
    
    * Added some Placeholder Material for Series Cards
    
    * Fixed Image gradient overlay
    
    Overlay would only be rendered properly in the Android Stuidoi Layout
    inspector but neither on an emulator, nor on a real device.
    
    Turns out, the problem was that the gradient was being passed as a simple view's
    background and tehrefor needed a fixed width pre-runtime.
    Moving the gradient as the background of a relative layout fixed the
    problem.
    
    Further reference:
    http://stackoverflow.com/questions/20804564/overlay-over-an-imageview-on-android
    
    * Fixed Layout for RecyclerView and Crads
    
    * Added ripple effect and focus state
    
    * Switched color vars from camelCase to snake_case
    
    Refactroed three of the color variables defined by the theme. Tested in
    the emulator and on a real device, both of which looked perfectly fine.
    
    We should keep these changes in mind though, just in case something gets
    messed up in the theme core later on, for this could be the reason for it.
    
    * Moved text colors to variables
    
    For the alpha, there is some calculation that needs to be done.
    
    A good guideline can be found
    here: http://stackoverflow.com/a/11286013/4181679
    I'll go through it on the example of the primary text, which has a color
    of #000000 and a opacity of 87%.
    
    First, we have to convert the 0.87 opacity into the alpha channel, which
    ranges from 0 to 255. Therefore: 255 * 0.87 = 221,85 -> 222.
    Then, we just convert this from base10 to base16 which is DE.
    We then prepend this to our color and get #DE000000.
    
    * Moved onClickListener in Adapter
    
    * Documentation and tidying up
    
    * Merge master at 3744fccb659fc74f28f6df560395b39a39481e28
    
    * Revert "Merge master at 3744fccb659fc74f28f6df560395b39a39481e28"
    
    This reverts commit d1a2e664904e3d80c096ee9ad572897828be2986.
    
    * Merge master at 3744fccb659fc74f28f6df560395b39a39481e28
    
    * This merge should be correct to the line

--- app/src/main/java/io/megaquiche/binge/ui/activities/MainActivity.java
+++ app/src/main/java/io/megaquiche/binge/ui/activities/MainActivity.java
@@ -1,13 +1,104 @@
-package io.megaquiche.binge;
+package io.megaquiche.binge.ui.activities;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import io.megaquiche.binge.R;
+import io.megaquiche.binge.pojo.SeriesDummy;
+import io.megaquiche.binge.ui.adapter.MainActivityAdapter;
+
+public class MainActivity extends AppCompatActivity implements MainActivityAdapter.SeriesAdapterInterface {
+
+    private static final String TAG = "MainActivity";
+    private Toolbar toolbar;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up the Toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.activity_main_title);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+
+        // Check if actionBar != null to evade the compiler warning
+        if(actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+
+        // Set StatusBar Color
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.color_primary_dark));
+
+        // Set up RecyclerView for Series Cards
+        mRecyclerView = (RecyclerView) findViewById(R.id.activity_main_recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // Create dummy data and fill the RecyclerView with it
+        List<SeriesDummy> seriesList  = createDummyData();
+        mAdapter = new MainActivityAdapter(this, seriesList, this);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_home, menu);
+
+        return true;
+    }
+
+    /**
+     * Creates dummy data to test the RecyclerView
+     * May be removed in later versions.
+     * @return A list with dummy data
+     */
+    private List<SeriesDummy> createDummyData() {
+
+        List<SeriesDummy> dummyList = new ArrayList<SeriesDummy>();
+
+        SeriesDummy bigBangTheory = new SeriesDummy(getDrawable(R.drawable.placeholder_bbt_title),
+                "The Big Bang Theory", "Doktor Proton", "S06E08");
+
+        SeriesDummy greysAnatomy = new SeriesDummy(getDrawable(R.drawable.placeholder_ga_title),
+                "Greys Anatomy", "Ach Oh Gott die Liebe", "S18E02");
+
+        SeriesDummy betterCallSaul = new SeriesDummy(getDrawable(R.drawable.placeholder_bcs_title),
+                "Better Call Saul", "Tuco", "S01E06");
+
+        dummyList.add(bigBangTheory);
+        dummyList.add(greysAnatomy);
+        dummyList.add(betterCallSaul);
+
+        return dummyList;
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        // TODO: Do things
     }
 }
---
+++ app/src/main/java/io/megaquiche/binge/ui/adapter/MainActivityAdapter.java
@@ -0,0 +1,105 @@
+package io.megaquiche.binge.ui.adapter;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import io.megaquiche.binge.R;
+import io.megaquiche.binge.pojo.SeriesDummy;
+
+/**
+ * Adapter for the RecyclerView in the MainActivity.
+ * All very straight forward.
+ *
+ * Created by plsr on 29/05/16.
+ */
+public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapter.ViewHolder> {
+
+    public static final String TAG = "MainActivityAdapter";
+
+    public interface SeriesAdapterInterface {
+        void onItemClick(int position);
+    }
+
+    private Context mContext;
+    private List<SeriesDummy> mSeries;
+    private SeriesAdapterInterface mInterface;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        CardView seriesCardView;
+        ImageView titleImage;
+        TextView seriesTitle;
+        TextView episodeTitle;
+        TextView episodeNumber;
+        ImageButton actionMarkWatched;
+
+        public ViewHolder(View v) {
+            super(v);
+            seriesCardView = (CardView) v.findViewById(R.id.series_card);
+            titleImage = (ImageView) v.findViewById(R.id.title_image);
+            seriesTitle = (TextView) v.findViewById(R.id.series_title);
+            episodeTitle = (TextView) v.findViewById(R.id.episode_title);
+            episodeNumber = (TextView) v.findViewById(R.id.episode_number);
+            actionMarkWatched = (ImageButton) v.findViewById(R.id.action_mark_watched);
+
+        }
+    }
+
+    public MainActivityAdapter (Context context, List<SeriesDummy> series, SeriesAdapterInterface rInterface) {
+        mContext = context;
+        mSeries = series;
+        mInterface = rInterface;
+    }
+
+
+    @Override
+    public MainActivityAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_series, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(MainActivityAdapter.ViewHolder holder, final int position) {
+        final SeriesDummy series = mSeries.get(position);
+        CardView seriesCardView = (holder).seriesCardView;
+        ImageView titleImage = (holder).titleImage;
+        TextView seriesTitle = (holder).seriesTitle;
+        TextView episodeTitle = (holder).episodeTitle;
+        TextView episodeNumber = (holder).episodeNumber;
+        ImageButton actionMarkWatched = (holder).actionMarkWatched;
+
+        titleImage.setImageDrawable(series.getTitleImage());
+        seriesTitle.setText(series.getSeriesTitle());
+        episodeTitle.setText(series.getEpisodeTitle());
+        episodeNumber.setText(series.getEpisodeCount());
+
+        // onClickListerner for ImageButton
+        actionMarkWatched.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // TODO: Mark latest unseen epsiode as seen and display next unseen Episode
+                Log.d(TAG, "actionMarkWatched onClick listener called in position " + position);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSeries.size();
+    }
+
+
+
+}
