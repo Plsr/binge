83e9ca79c5fcd79a5555cbc64a4f4e7bcfba86f9
plsr <christianpoplawski@gmail.com>
2017-02-12 14:03:05 +0100

    Basic Toolbar Setup
    Back Button working, the overflow menu icon is still a dummy since we did not really define what options will be present in the overflow menu.

--- app/src/main/java/io/megaquiche/binge/ui/activities/SeriesDetailActivity.java
+++ app/src/main/java/io/megaquiche/binge/ui/activities/SeriesDetailActivity.java
@@ -1,9 +1,15 @@
 package io.megaquiche.binge.ui.activities;
 
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,6 +25,18 @@ public class SeriesDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.series_detail);
 
+        // Set up the Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+
+        // Check if actionBar != null to evade the compiler warning
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+
         Intent intent = getIntent();
         SeriesDummy seriesDummy =  intent.getParcelableExtra("SERIES_DUMMY");
         Log.d(TAG, seriesDummy.getSeriesTitle()); // DEBUG
@@ -28,5 +46,29 @@ public class SeriesDetailActivity extends AppCompatActivity {
 
         seriesTitle.setText(seriesDummy.getSeriesTitle());
         titleImage.setImageDrawable(getDrawable(seriesDummy.getTitleImageIdentifier()));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            // Respond to the overflow menu icon
+            case R.id.action_overflow_menu:
+                // TODO: Show the overflow menu.
+                // menu items not yet defined.
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_options, menu);
+        return true;
     }
 }
